d97a1fc7a878ee94ae1bb949c51c958ce24e6375
Cas van der Gun <cas@vandergun.com>
2024-11-15 19:46:57 +0100

    example

--- src/lib/seal.ts
+++ src/lib/seal.ts
@@ -2,29 +2,33 @@
 
 import { defaults, seal, unseal, type SealOptions } from "iron-webcrypto";
 
-import { sessionOptions } from "./options";
 import { SessionData } from "./session";
+
+let AUTH_SESSION_SECRET: string = "PlaceholderThatNeedsToBe32MinimumCharactersLongAndShouldBeReplacedWithARealSecret";
+let AUTH_SESSION_DURATION: number = 60 * 60 * 24 * 7; 
 
 
 const options: SealOptions = {
   ...defaults,
-  ttl: sessionOptions.duration * 1000,
+  ttl: AUTH_SESSION_DURATION * 1000,
 };
 
 
 export async function sealSession(unsealedSession: SessionData): Promise<string> {
+  "use server";
 
   const data = JSON.stringify(unsealedSession);
 
-  const sealedSession = await seal(crypto, data, sessionOptions.secret, options);
+  const sealedSession = await seal(crypto, data, AUTH_SESSION_SECRET, options);
   
   return sealedSession;
 }
 
 
 export async function unsealSession(sealedSession: string): Promise<SessionData> {
+  "use server";
 
-  const data = await unseal(crypto, sealedSession, sessionOptions.secret, options) as string;
+  const data = await unseal(crypto, sealedSession, AUTH_SESSION_SECRET, options) as string;
 
   const unsealedSession = JSON.parse(data) as SessionData;
 
